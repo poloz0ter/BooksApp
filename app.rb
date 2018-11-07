af2e91afc8608c0c16f1e384d17ee56034de8bd7
poloz0ter <polozoter@gmail.com>
2018-11-07 19:35:47 +1100

    inclmplete first Sinatra app

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'movie'
+
+get('/movies') do
+  @movies = []
+  @movies[0] = Movie.new
+  @movies[0].title = 'Jaws'
+  @movies[1] = Movie.new
+  @movies[1].title = 'Alien'
+  @movies[2] = Movie.new
+  @movies[2].title = 'Terminator 2'
+  erb :index
+end
+
+get('/movies/new') do
+  erb :new
+end
+
+post('/movies/create') do
+  @movie = Movie.new
+  @movie.title = params['title']
+  @movie.director = params['director']
+  @movie.year = params['year']
+end
---
+++ yaml_test.rb
@@ -0,0 +1,21 @@
+require './lib/movie'
+require 'yaml/store'
+
+store = YAML::Store.new('test.yml')
+
+first_movie = Movie.new
+first_movie.title = 'Old Borov'
+first_movie.director = 'Glad Valacas'
+first_movie.year = 2007
+
+second_movie = Movie.new
+second_movie.title = 'Star Boy 3'
+second_movie.director = 'Bobby Star'
+second_movie.year = 2001
+
+store.transaction do
+  store['Old Borov'] = first_movie
+  store['Star Boy 3'] = second_movie
+  
+  p store['Old Borov']
+end
